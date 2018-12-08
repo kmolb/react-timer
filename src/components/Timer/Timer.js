f48cae0f874241a7e45fb8aadb28772d60eaa05b
Kamil Olber <olb.kamil@gmail.com>
2018-12-08 09:23:34 +0100

    2 struktura plików,początek

---
+++ src/components/Timer/Timer.js
@@ -0,0 +1,14 @@
+import React, {
+  Component
+} from 'react';
+import './Timer.css';
+
+class Timer extends Component {
+  render() {
+    return ( <
+      div> Timer </div>
+    );
+  }
+}
+
+export default Timer;
